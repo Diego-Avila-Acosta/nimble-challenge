f2e0a18a6ccd6e8b0d184bd3fd0ea467862f7187
Diego Avila <aviladiego2009@gmail.com>
2026-02-18 20:10:16 -0300

    Added Job component

---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,46 @@
+import Swal from "sweetalert2";
+
+function Job ({id, title, postApplication}) {
+    const [repoUrl, setRepoUrl] = useState("")
+
+    const handleInput = (e) => {
+        setRepoUrl(() => e.target.value);
+    }
+
+    const handleSubmit = () => {
+        if(repoUrl.trim() === "") {
+            Swal.fire({
+                icon: "error",
+                title: "Error al enviar postulación",
+                text: "Necesita ingresar una url de repositorio valido"
+            })
+            return;
+        }
+
+        postApplication(repoUrl, id);
+    }
+
+    return (
+        <tr key={id} style={{cursor: 'pointer'}}>
+            <td className="ps-4 fw-bold text-dark">
+                {title}
+            </td>
+            <td>
+                <input type="text" name="repoUrl" value={repoUrl} onChange={handleInput}/>
+            </td>
+            <td className="pe-4 text-end">
+                <div className="d-flex justify-content-end gap-2">
+                    <button 
+                        className="btn btn-sm btn-outline-primary border-0 bg-primary bg-opacity-10 text-primary" 
+                        title="Submit"
+                        onClick={() => handleSubmit}
+                    >
+                        <i className="bi bi-eye-fill">Submit</i>
+                    </button>
+                </div>
+            </td>
+        </tr>
+    )
+}
+
+export default Job;
