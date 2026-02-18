20fb0f3f7a8d0828a62ebe65a5f3f4c33cfb30dd
Diego Avila <aviladiego2009@gmail.com>
2026-02-18 20:21:24 -0300

    Added Main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { candidateService } from "../../services/api";
+import JobList from "../JobsList/JobsList";
+import CandidateForm from "../CandidateForm/CandidateForm";
+import Swal from "sweetalert2";
+
+function Main() {
+    const [candidate, setCandidate] = useState({});
+    
+    const postApplication = (repoUrl, jobId) => {
+        if(!candidate.uuid) {
+            Swal.fire({
+                icon: "error",
+                title: "Error al enviar postulación",
+                text: "Necesita ingresar un candidato valido"
+            })
+            return;
+        }
+
+        const payload = {
+            uuid: candidate.uuid,
+            jobId,
+            candidateId: candidate.candidateId,
+            repoUrl
+        }
+
+        candidateService.post(payload).then((response) => {
+            if(response.status == 200) {
+                Swal.fire({
+                    position: "bottom-right",
+                    icon: "success",
+                    title: "Se envió las postulación correctamente",
+                    showConfirmButton: false,
+                    timer: 1000
+                });
+            }
+        }).catch((e) => {
+            console.log(e.toJSON())
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Algo salió mal en la petición..."
+            })
+        })
+    }
+
+    return (
+        <div>
+            <CandidateForm 
+                candidate={candidate}
+                setCandidate={setCandidate}
+            />
+            <JobList
+                postApplication={postApplication}
+            />
+        </div>
+    );
+
+}
+
+export default Main;
